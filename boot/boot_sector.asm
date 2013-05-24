434369fe7e38e734dfc2150210ab151be2705fb5
Geng Shuang <gengs@neusoft.com>
2013-05-24 11:11:10 +0800

    判断 loader 是否存在

--- boot/boot_sector.asm
+++ boot/boot_sector.asm
@@ -13,9 +13,9 @@ START:
     mov    di, BOOT_OFFSET
     mov    cx, 256
     rep    movsw
-    jmp    BOOT_SEG:GOON
+    jmp    BOOT_SEG:GO
 
-GOON:
+GO:
     mov    ax, cs
     mov    ds, ax
     mov    es, ax
@@ -48,10 +48,40 @@ GOON:
     mov    es, ax
     mov    dx, 0000h
     mov    cx, 0002h
-    mov    bx, LOADER_OFF
+    mov    bx, LOADER_OFFSET
     mov    ah, 02h
-    mov    al, LOADER_SZE
+    mov    al, LOADER_SECTOR
     int    13h
+    pop    es
+
+    push   es
+    mov    ax, LOADER_SEG
+    mov    es, ax,
+    mov    ax, LOADER_OFFSET + LOADER_SIZE - 5
+    mov    di, ax
+    mov    si, loader_mgic
+    mov    cx, 5
+    cld
+    CHECK_LOADER:
+        cmp    cx, 0
+        jz     LOADER_FOUNT
+        dec    cx
+        lodsb
+        cmp    al, byte [es:di]
+        jz     GO_ON
+
+        push   msg_loader_err
+        push   msg_loader_err_len
+        push   0200h
+        call   disp_str
+        add    esp, 6
+        jmp    $
+
+        GO_ON:
+            inc    di
+            jmp    CHECK_LOADER
+
+        LOADER_FOUNT:
     pop    es
 
     push   msg_ready
@@ -60,14 +90,18 @@ GOON:
     call   disp_str
     add    esp, 6
 
-    jmp    LOADER_SEG:LOADER_OFF
+    jmp    LOADER_SEG:LOADER_OFFSET
 
 %include "io.inc"
 
-msg_boot:       db   "Booting ..."
-msg_boot_len    equ  $ - msg_boot
-msg_ready:      db   "Ready"
-msg_ready_len   equ  $ - msg_ready
+msg_boot:            db   "Booting ..."
+msg_boot_len         equ  $ - msg_boot
+msg_ready:           db   "Ready"
+msg_ready_len        equ  $ - msg_ready
+msg_loader_err:      db   "loader not found."
+msg_loader_err_len   equ  $ - msg_loader_err
+
+loader_mgic:    LOADER_MAGIC
 
 times  510 - ($ - $$)  db  0
 dw  0xaa55
--- boot/loader.asm
+++ boot/loader.asm
@@ -1,7 +1,7 @@
 
 %include "defs.inc"
 
-org    LOADER_OFF
+org    LOADER_OFFSET
 jmp    START
 
 START:
@@ -9,20 +9,20 @@ START:
     mov    ds, ax
     mov    es, ax
     mov    ss, ax
-    mov    esp, LOADER_OFF
+    mov    sp, LOADER_OFFSET
 
-    push   msg_load
-    push   msg_load_len
+    push   msg_load_start
+    push   msg_load_start_len
     push   0300h
-    call   disp_str1
+    call   disp_str
     add    esp, 6
 
     jmp    $
 
 %include "io.inc"
 
-msg_load:       db   "Loading ..."
-msg_load_len    equ  $ - msg_load
+msg_load_start:     db   "Loading ..."
+msg_load_start_len  equ  $ - msg_load_start
 
-times  512 * LOADER_SZE - 4 - ($ - $$)  db  0
-dd  081192h
+times  LOADER_SIZE - 5 - ($ - $$)  db  0
+LOADER_MAGIC
